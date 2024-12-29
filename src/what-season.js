888febf6d1a42197f8b7183cea217e3ff91f6f1e
Aliaksandr Datskevich <datskevich.a@gmail.com>
2024-12-29 22:28:07 +0300

    feat: what-season

--- src/what-season.js
+++ src/what-season.js
@@ -11,10 +11,46 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSeason(date) {
+  if (date === undefined) {
+    return 'Unable to determine the time of year!'
+  }
+  if (!(date instanceof Date) || (date.hasOwnProperty('toString'))) {
+    throw new Error('Invalid date!');
+  }
+  let month = date.getMonth();
+  switch (month) {
+    case 11:
+    case 0:
+    case 1:
+      return 'winter';
+    case 2:
+    case 3:
+    case 4:
+      return 'spring';
+    case 5:
+    case 6:
+    case 7:
+      return 'summer';
+    case 8:
+    case 9:
+    case 10:
+      return 'autumn';
+  } 
 }
+
+// const fakeDate = {
+//   toString() {
+//       return Date.prototype.toString.call(new Date());
+//   },
+//   [Symbol.toStringTag]: 'Date'
+// };
+
+// Object.setPrototypeOf(fakeDate, Object.getPrototypeOf(new Date()));
+
+// getSeason(fakeDate);
+// getSeason('foo');
+// getSeason(new Date(1664, 1, 17, 13, 41, 10, 345));
 
 module.exports = {
   getSeason
